38deab9e9c52b1f9a01c9728893ea2d381b5f713
Manousos Linardakis <it22064@hua.gr>
2022-12-12 00:16:19 +0200

    test

---
+++ src/main/java/gr/hua/dit/dissys/DissysApplication.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.dissys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DissysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DissysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/hua/dit/dissys/DissysApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.dissys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DissysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
