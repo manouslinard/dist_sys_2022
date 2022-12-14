d4650017c7382458c3230483a218b25dcfed44df
manouslinard <it22064@hua.gr>
2022-12-14 18:43:47 +0200

    implemented some of lessors backend

--- src/main/java/gr/hua/dit/dissys/controller/LessorController.java
+++ src/main/java/gr/hua/dit/dissys/controller/LessorController.java
@@ -1,12 +1,16 @@
 package gr.hua.dit.dissys.controller;
 
+import gr.hua.dit.dissys.entity.Lease;
 import gr.hua.dit.dissys.entity.Lessor;
 import gr.hua.dit.dissys.entity.Tenant;
+import gr.hua.dit.dissys.repository.LeaseRepository;
 import gr.hua.dit.dissys.service.LessorService;
 import gr.hua.dit.dissys.service.TenantService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -21,12 +25,47 @@ public class LessorController {
     @Autowired
     private TenantService tenantService;
 
+    @Autowired
+    private LeaseRepository leaseRepo;
+    
     @GetMapping("/getAllTenants")
-    public List<Tenant> getAll()
+    public List<Tenant> getAllTenants()
     {
     	return tenantService.getTenants();
     }
 
+    @GetMapping("/{id}/leases")
+    public List<Lease> getAllLeases(@PathVariable int id) {
+        Lessor l = (Lessor) lessorService.findLessor(id);
+        if (l == null) {
+        	throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "entity not found"
+             );
+        }
+        return l.getLeases();
+    }
+   
+    @GetMapping("/{id}/leases/{lid}")
+    public Lease getLessorLease(@PathVariable int id, @PathVariable int lid) {
+        List<Lease> lessorLeases = getAllLeases(id);
+        for(Lease lease : lessorLeases) {
+        	if(lease.getId() == lid) {
+        		return lease;
+        	}
+        }
+       	throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "entity not found"
+        );
+    }
+    
+    @DeleteMapping("{id}/leases/{lid}")
+    public void deleteLease(@PathVariable int id, @PathVariable int lid) {
+    	Lease lease = getLessorLease(id, lid);
+    	leaseRepo.delete(lease);
+    }
+    
+    
+    
     @PostMapping("")
     public Lessor save(@Valid @RequestBody Lessor lessor) {
         lessor.setId(0);
