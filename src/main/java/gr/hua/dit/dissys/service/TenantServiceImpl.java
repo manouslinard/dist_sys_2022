c06ee2b6f996cfc7a27919fa0d147e4f21a971e7
kazakos <IT22033@HUA.GR>
2022-12-20 13:46:56 +0200

    impl tenant service(users)

--- src/main/java/gr/hua/dit/dissys/service/TenantServiceImpl.java
+++ src/main/java/gr/hua/dit/dissys/service/TenantServiceImpl.java
@@ -1,12 +1,17 @@
 package gr.hua.dit.dissys.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import gr.hua.dit.dissys.entity.Role;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import gr.hua.dit.dissys.entity.UserRegistration;
 import gr.hua.dit.dissys.repository.UserRepository;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 
@@ -14,33 +19,57 @@ import javax.transaction.Transactional;
 public class TenantServiceImpl implements TenantService{
 
 	@Autowired
-    private UserRepository tenantRepository;
-	
+    private UserRepository userRepository;
+
+	@SuppressWarnings("unlikely-arg-type")
+	private boolean isTenant(Set<Role> userRoles) {
+		for(Role r:userRoles) {
+			if(r.getName().equals("ROLE_TENANT")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	@Override
 	@Transactional
 	public List<UserRegistration> getTenants() {
-        return tenantRepository.findAll();
+		List<UserRegistration> users = userRepository.findAll();
+		List<UserRegistration> tenants = new ArrayList<>();
+
+		for(UserRegistration u:users) {
+			if(isTenant(u.getRoles())) {
+				tenants.add(u);
+			}
+		}
+
+		return tenants;
 	}
 
 	@Override
 	@Transactional
 	public void saveTenant(UserRegistration tenant) {
-        tenantRepository.save(tenant);		
+        userRepository.save(tenant);
 	}
 
 
-    @Override
-    @Transactional
-    public UserRegistration findTenant(String username) {
-    	return tenantRepository.findByUsername(username).get();
-    }
+	@Override
+	@Transactional
+	public UserRegistration findTenant(String username) {
+		List<UserRegistration> tenants = getTenants();
+		for(UserRegistration t: tenants) {
+			if(t.getUsername().equals(username)) {
+				return t;
+			}
+		}
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found");
+	}
 
     @Override
     @Transactional
     public void deleteTenant(String username) {
-    	if (tenantRepository.existsByUsername(username)) {
-    		UserRegistration u = tenantRepository.findByUsername(username).get();
-    		tenantRepository.delete(u);
-    	}
-    }
+		UserRegistration tenant = findTenant(username);
+		userRepository.delete(tenant);
+	}
 }
+
