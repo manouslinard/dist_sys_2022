ae6503c9b29a8022072ae40b37973e40fdb2eee4
manouslinard <it22064@hua.gr>
2022-12-20 15:27:24 +0200

    fix bug in findLessor/Tenant

--- src/main/java/gr/hua/dit/dissys/controller/AuthController.java
+++ src/main/java/gr/hua/dit/dissys/controller/AuthController.java
@@ -97,19 +97,19 @@ public class AuthController {
         Set<Role> roles = new HashSet<>();
 
         if (strRoles == null) {
-            Role userRole = roleRepository.findByName(ERole.ROLE_TENANT)
+            Role userRole = roleRepository.findByName(ERole.ROLE_LESSOR)
                     .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
             roles.add(userRole);
         } else {
             strRoles.forEach(role -> {
                 switch (role) {
-                    case "admin":
+                    case "ROLE_ADMIN":
                         Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
                                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
                         roles.add(adminRole);
 
                         break;
-                    case "lessor":
+                    case "ROLE_LESSOR":
                         Role modRole = roleRepository.findByName(ERole.ROLE_LESSOR)
                                 .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
                         roles.add(modRole);
