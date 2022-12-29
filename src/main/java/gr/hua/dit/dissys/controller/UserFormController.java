1ac369475a1195dd17a8787821a1f02ab99476df
KostasKatsaras <it22045@hua.gr>
2022-12-29 14:29:17 +0200

    Impl-Admin-Form

--- src/main/java/gr/hua/dit/dissys/controller/UserFormController.java
+++ src/main/java/gr/hua/dit/dissys/controller/UserFormController.java
@@ -10,6 +10,7 @@ import gr.hua.dit.dissys.entity.ERole;
 import gr.hua.dit.dissys.entity.Lease;
 import gr.hua.dit.dissys.entity.Role;
 import gr.hua.dit.dissys.repository.RoleRepository;
+import gr.hua.dit.dissys.service.AdminService;
 import gr.hua.dit.dissys.service.LeaseService;
 import gr.hua.dit.dissys.service.LessorService;
 import gr.hua.dit.dissys.service.TenantService;
@@ -40,6 +41,9 @@ public class UserFormController {
     
     @Autowired
     private RoleRepository roleRepository;
+    
+    @Autowired
+    private AdminService adminService;
     
     @GetMapping("/")
     public String index() {
@@ -151,5 +155,18 @@ public class UserFormController {
 		}
 	}
     
+	  	@PostMapping(path = "/adminform")
+	    public String saveAdmin(@ModelAttribute("admin") AverageUser admin) {
+	    	setBlankAttr(admin);
+	        adminService.saveAdmin(admin);
+	        return "redirect:/";
 
+	    }
+	
+	    @GetMapping("/adminform")
+	    public String showAdminForm(Model model) {
+	    	AverageUser admin = new AverageUser();
+	        model.addAttribute("admin", admin);
+	        return "add-admin";
+	    }
 }
