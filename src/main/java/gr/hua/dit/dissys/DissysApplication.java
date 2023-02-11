c3ad0a31089a29eede4d6ba3a0b0cb96c2785720
manouslinard <it22064@hua.gr>
2023-02-12 00:22:47 +0200

    added test images+files+configed paths

--- src/main/java/gr/hua/dit/dissys/DissysApplication.java
+++ src/main/java/gr/hua/dit/dissys/DissysApplication.java
@@ -2,12 +2,23 @@ package gr.hua.dit.dissys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
-public class DissysApplication {
+public class DissysApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DissysApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DissysApplication.class, args);
 	}
 
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+	}
 }
